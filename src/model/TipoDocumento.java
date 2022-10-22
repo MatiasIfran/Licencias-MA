e6376a5631ede28137e6990677f4e765b319ad42
Matias Ifran <ifran2855@gmail.com>
2022-10-22 11:21:39 -0300

    get and set tipoDocumento

--- src/model/TipoDocumento.java
+++ src/model/TipoDocumento.java
@@ -1,5 +1,15 @@
 package model;
 
 public class TipoDocumento {
+	
+	private String tipoDocumento;
+
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
 
 }
